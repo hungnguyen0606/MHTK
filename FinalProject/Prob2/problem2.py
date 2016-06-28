95e48d8c3fd7b2047bb3797acec9485294438d23
HungNguyen <nguyenphanmanhhung@gmail.com>
2016-06-28 11:38:17 +0700

    Update prob2: data info, graph, report

--- FinalProject/Prob2/problem2.py
+++ FinalProject/Prob2/problem2.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn import svm
 from sklearn import linear_model
 from sklearn import preprocessing
@@ -70,9 +72,63 @@ def makeLinear(X):
     finally:
         pass
 
+def getInfo(X):
+    #print X
+    try:
+
+        mean = np.mean(X, axis = 0)
+        assert mean.shape[0] == 2, 'Mean err'
+        std = np.std(X, axis = 0)
+        assert std.shape[0] == 2, 'Std err'
+        minn = np.min(X, axis = 0)
+        maxx = np.max(X, axis = 0)
+        assert minn.shape[0] == 2 and maxx.shape[0] == 2, 'Min or Max err'
+
+        print 'Mean X: %f.\nStd X: %f'%(mean[0], std[0])
+        print 'Min X: %f.\nMax X: %f\nRange X: %f'%(minn[0], maxx[0], maxx[0] - minn[0])
+        print ''
+        print 'Mean Y: %f.\nStd Y: %f'%(mean[1], std[1])
+        print 'Min Y: %f.\nMax Y: %f\nRange Y: %f'%(minn[1], maxx[1], maxx[1] - minn[1])
+        #-------------------------------------------------------------------
+        x1 = np.linspace(0.0, 5.0)
+        x2 = np.linspace(0.0, 2.0)
+
+        y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+        y2 = np.cos(2 * np.pi * x2)
+
+        # plt.subplot(2, 1, 1)
+        # plt.plot(x1, y1, 'ko-')
+        # plt.title('A tale of 2 subplots')
+        # plt.ylabel('Damped oscillation')
+        # plt.xlabel('Hello')
+        # plt.subplot(2, 1, 2)
+        # plt.plot(x2, y2, 'r.-')
+        # plt.xlabel('time (s)')
+        # plt.ylabel('Undamped')
+
+        # plt.show()
+        plt.subplot(2,1,1)
+        plt.hist(X[:, 0], bins = 20, color = 'b')
+        plt.ylabel('Count')
+        plt.xlabel('X')
+
+        plt.subplot(2,1,2)
+        plt.hist(X[:, 1], bins = 20, color = 'r')
+        plt.ylabel('Count')
+        plt.xlabel('Y')
+        plt.show()
+    except Exception as err:
+        print err
+    finally:
+        pass
+
 def main():
-    generateData(40000, 'input40000.txt')
-    listInput = ['input200.txt', 'input2000.txt', 'input40000.txt']
+    #generateData(200, 'input200.txt')
+    N, x, z = loadData('input200.txt')
+    #getInfo(x)
+
+    
+    listInput = ['input200.txt']
     try:
 
         for i in range(len(listInput)):
@@ -103,7 +159,10 @@ def main():
 
                 reg_acc.append(np.sum(nz==z[test_id])*1.0/test_id.size)
 
+            print svm_acc
+            print reg_acc
             print np.mean(svm_acc)*100, np.mean(reg_acc)*100
+            print np.std(svm_acc), np.std(reg_acc)
     except Exception as err:
         print err
     finally:
